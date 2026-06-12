4c28aefde38dc9b5a62226abac07ef48e365b5fc
moonish <moonishtamilarasu@gmail.com>
2026-06-12 14:51:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSurprise() {
+
+    let surprise = document.getElementById("surprise");
+
+    if(surprise.style.display === "block"){
+        surprise.style.display = "none";
+    } else {
+        surprise.style.display = "block";
+    }
+
+}
