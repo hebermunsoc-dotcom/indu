4d22aa9b82e62b8b4fa0fe254b9973f3f9a3a8bc
moonish <moonishtamilarasu@gmail.com>
2026-06-12 19:42:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-const hiddenElements =
-document.querySelectorAll(".hidden");
-
-const observer =
-new IntersectionObserver((entries)=>{
-
-entries.forEach((entry)=>{
-
-if(entry.isIntersecting){
-entry.target.classList.add("show");
-}
-
-});
-
-});
-
-hiddenElements.forEach((el)=>{
-observer.observe(el);
-});
-
-
 document
 .getElementById("celebrateBtn")
 .addEventListener("click",()=>{
@@ -29,9 +8,40 @@ document.getElementById("confettiText");
 text.innerHTML = `
 🎉 🎊 🎉 🎊 🎉
 <br>
-UMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!
+UMAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHH ❤️
 <br>
 🎂 ❤️ 🌟
 `;
+
+for(let i=0;i<30;i++){
+
+const heart =
+document.createElement("div");
+
+heart.innerHTML = "❤️";
+
+heart.style.position = "fixed";
+heart.style.left = Math.random()*100 + "vw";
+heart.style.top = "-20px";
+heart.style.fontSize = "2rem";
+heart.style.zIndex = "9999";
+
+document.body.appendChild(heart);
+
+heart.animate(
+[
+{transform:"translateY(0px)"},
+{transform:"translateY(110vh)"}
+],
+{
+duration:3000 + Math.random()*2000
+}
+);
+
+setTimeout(()=>{
+heart.remove();
+},5000);
+
+}
 
 });
